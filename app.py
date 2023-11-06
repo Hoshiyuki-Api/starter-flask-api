25fcefeb48025dbd3862e90bdfb3f93b7653b93b
AmmarXploit <104218064+AmmarrBN@users.noreply.github.com>
2023-11-06 20:58:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask
+from flask import Flask,request,make_response,jsonify
+from flask_restful import Resource,Api
+import jwt,datetime
+from functools import wraps
 from fake_useragent import UserAgent
-        
-import os
 
-app = Flask(__name__)
+app=Flask(__name__)
 api=Api(app)
+app.config["SECRET_KEY"] = "inirahasianegara"
+
+def kunci_halaman(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token=request.args.get('token')
+        if not token:
+            return make_response(jsonify({
+                "mssg":"Token not valid !!!"
+            }), 404)
+
+        try:
+            output=jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])
+        except:
+            return make_response(jsonify({"mssg":"Token Not Valid !!!!!"}))
+        return f(*args, **kwargs)
+    return decorator
+
+class LoginUser(Resource):
+    def post(self):
+        user=request.form.get('username')
+        pas=request.form.get('password')
+
+        if user and pas == 'superadmin':
+            token=jwt.encode(
+                {
+                    "username":user,
+                    "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+                }, app.config["SECRET_KEY"],algorithm="HS256"
+            )
+            return jsonify({
+                "token": token,
+                "mssg": "Success Login",
+                "response code": 200
+            })
+            
+        return jsonify({"mssg": "Login terlebih dahulu !"})
+
+class Dashboard(Resource):
+    @kunci_halaman
+    def get(self):
+        return jsonify({
+            "mssg":"Ini halaman web yang hanya bisa di akses setelah login"
+        })
+
+class HomePage(Resource):
+    def get(self):
+        return jsonify({"mssg":"Ini Adalah halaman public"})
 
 class RandomUa(Resource):
     def get(self):
@@ -15,6 +64,11 @@ class RandomUa(Resource):
             "response code": 200,
             "creator": "Ammar-Excuted"
         })
-api.add_resource(RandomUa, "/uar", methods=["GET"])
+
+
+api.add_resource(LoginUser, "/apikey/login", methods=["POST"])
+api.add_resource(Dashboard, "/apikey/dashboard", methods=["GET"])
+api.add_resource(HomePage, "/apikey", methods=["GET"])
+api.add_resource(RandomUa, "/api/user-agent", methods=["GET"])
 if __name__ == "__main__":
     app.run(debug=True)
