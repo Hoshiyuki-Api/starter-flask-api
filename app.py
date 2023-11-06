7723116d23a921acd6263844f2da2d3c43844623
AmmarXploit <104218064+AmmarrBN@users.noreply.github.com>
2023-11-06 20:50:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from fake_useragent import UserAgent
+        
 import os
 
 app = Flask(__name__)
+api=Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, world!'
+class RandomUa(Resource):
+    def get(self):
+        useragent=UserAgent()
+        ua=useragent.random
+        return ({
+            "user-agent": ua,
+            "response code": 200,
+            "creator": "Ammar-Excuted"
+        })
+api.add_resource(RandomUa, "/uar", methods=["GET"])
+if __name__ == "__main__":
+    app.run(debug=True)
