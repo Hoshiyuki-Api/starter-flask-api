8d2b147191e4a85381fb84c6fee1724d52c3b878
AmmarXploit <104218064+AmmarrBN@users.noreply.github.com>
2023-11-25 23:18:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,make_response,jsonify,redirect,url_for,render_template
 from flask_restful import Resource,Api,reqparse
-import jwt,datetime,requests,json,valodators
+import jwt,datetime,requests,json,validators
 from functools import wraps
 from fake_useragent import UserAgent
 
